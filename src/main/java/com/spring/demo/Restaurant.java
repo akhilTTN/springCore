bbe892ce8fd45a50e5069703c500909f9e00c99c
akhilTTN <akhil@tothenew.com>
2017-04-12 16:20:56 +0530

    changed code

--- src/main/java/com/spring/demo/Restaurant.java
+++ src/main/java/com/spring/demo/Restaurant.java
@@ -1,23 +1,30 @@
 package com.spring.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  * Created by akhil on 28/3/17.
  */
 public class Restaurant {
+
+    @Autowired
     HotDrink hotDrink;
 
     public Restaurant() {
+    }
+
+    //    @Autowired
+    public Restaurant(HotDrink hotDrink) {
+        this.hotDrink = hotDrink;
     }
 
     public HotDrink getHotDrink() {
         return hotDrink;
     }
 
+    /*@Autowired
+    @Required*/
     public void setHotDrink(HotDrink hotDrink) {
-        this.hotDrink = hotDrink;
-    }
-
-    public Restaurant(HotDrink hotDrink) {
         this.hotDrink = hotDrink;
     }
 }
